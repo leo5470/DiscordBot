dcaaf854df2e6cf9c9f93f2d29b5431653741b5c
leo5470 <leo5470@gmail.com>
2021-11-23 17:17:45 +0800

    New function/ video

--- MainApp.py
+++ MainApp.py
@@ -23,4 +23,8 @@ async def on_message(message):
     elif message.content.startswith('$memes'):
         pic = Memes.get()
         await message.channel.send(file=discord.File(pic))
+    elif message.content.startswith('$venom'):
+        await message.channel.send("假猛毒")
+        vid = Memes.venom()
+        await message.channel.send(file=discord.File(vid))
 client.run('OTAwNzI2NTkzOTQ3ODQ0NjU5.YXFhAg.-wJ99DG_2l0sV4AcmT8kjz8zBFA')
--- Memes.py
+++ Memes.py
@@ -1,8 +1,11 @@
 import os, random
+DIR = os.path.dirname(os.path.abspath(__file__))
 def get():
-    DIR = os.path.dirname(os.path.abspath(__file__))
     PIC_DIR = os.path.join(DIR, 'memes')
     files = os.listdir(PIC_DIR)
     target = random.choice(files)
     target = os.path.join(PIC_DIR, target)
     return target
+def venom():
+    VID_DIR = os.path.join(DIR, 'videos', 'Venom.mp4')
+    return VID_DIR
