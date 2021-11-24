9a3b9b6a47631ae4b09c32a380636c6bfc85c324
leo5470 <leo5470@gmail.com>
2021-11-24 18:29:31 +0800

    Update MainApp.py

--- MainApp.py
+++ MainApp.py
@@ -16,9 +16,7 @@ async def on_message(message):
     if message.author == client.user:
         return
     mention = message.author.mention
-    if message.content.startswith('lc!about'):
-        await message.channel.send('https://www.instagram.com/iamleocheng/')
-    elif message.content.startswith('lc!jokes'):
+    if message.content.startswith('lc!jokes'):
         joke_question, joke_answer = Jokes.tell()
         await message.reply("{}\n\n{}".format(joke_question, joke_answer))
     elif message.content.startswith('lc!memes'):
@@ -33,6 +31,10 @@ async def on_message(message):
 jokes：隨機產生一個笑話
 memes：隨機傳一張梗圖
 更多功能日後上線（當然，如果我有空的話）""")
+
+@bot.command()
+async def about(ctx):
+    await ctx.channel.send('https://www.instagram.com/iamleocheng/')
 
 @bot.command()
 async def hello(ctx):
@@ -50,7 +52,7 @@ async def ban(ctx, member : discord.Member, *, reason = None):
         reason = "過於智障"
     message = f"You have been banned from {ctx.guild.name} for {reason}"
     await member.send(message)
-    await ctx.channel.send(f"{member}已被Relaxing234永久禁言")
+    await ctx.channel.send(f"{member}已被relaxing234永久禁言")
     
 
 bot.run('OTAwNzI2NTkzOTQ3ODQ0NjU5.YXFhAg.-wJ99DG_2l0sV4AcmT8kjz8zBFA')
