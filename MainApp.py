92ec77e683ed3733bfdafe7c402a206c90477bdf
leo5470 <leo5470@gmail.com>
2021-11-24 11:07:06 +0800

    Revert "Update MainApp.py"
    This reverts commit 25a48b9d841e4a6caeb37b772db7bc290c3b3f31.

--- MainApp.py
+++ MainApp.py
@@ -18,12 +18,12 @@ async def on_message(message):
 @bot.command()
 async def hello(ctx):
     mention = ctx.author.mention
-    await ctx.send("Hello, {}".format(mention))
+    await ctx.channel.send("Hello, {}".format(mention))
 
 @bot.command()
 async def about(ctx):
-    await ctx.send('Author: https://www.instagram.com/iamleocheng/')
-    await ctx.send("""以lc!作為開頭呼叫
+    await ctx.channel.send('Author: https://www.instagram.com/iamleocheng/')
+    await ctx.channel.send("""以lc!作為開頭呼叫
 jokes：隨機產生一個笑話
 memes：隨機傳一張梗圖
 更多功能日後上線（當然，如果我有空的話）""")
@@ -31,31 +31,31 @@ memes：隨機傳一張梗圖
 @bot.command()
 async def jokes(ctx):
     joke_question, joke_answer = Jokes.tell()
-    await ctx.reply("{}\n\n{}".format(joke_question, joke_answer))
+    await ctx.channel.reply("{}\n\n{}".format(joke_question, joke_answer))
 
 @bot.command()
 async def memes(ctx):
     pic = Memes.get()
-    await ctx.send(file=discord.File(pic))
+    await ctx.channel.send(file=discord.File(pic))
 
 @bot.command()
 async def venom(ctx):
     await ctx.channel.send("假猛毒")
     vid = Memes.venom()
-    await ctx.send(file=discord.File(vid))
+    await ctx.channel.send(file=discord.File(vid))
 
 @bot.command()
 @commands.has_role("Admin")
 async def ban(ctx, member : discord.Member, *, reason = None):
     await member.ban(reason = reason)
-    if member is None or member == ctx.message.author:
-        await ctx.send("You cannot ban yourself")
+    if member == None or member == ctx.message.author:
+        await ctx.channel.send("You cannot ban yourself")
         return
-    if reason is None:
+    if reason == None:
         reason = "過於智障"
     message = f"You have been banned from {ctx.guild.name} for {reason}"
     await member.send(message)
-    await ctx.send(f"{member}已被Relaxing234永久禁言")
+    await ctx.channel.send(f"{member}已被Relaxing234永久禁言")
     
 
 bot.run('OTAwNzI2NTkzOTQ3ODQ0NjU5.YXFhAg.-wJ99DG_2l0sV4AcmT8kjz8zBFA')
