2a6a82885a83e674bef5b7d1514524af2015c5ac
leo5470 <leo5470@gmail.com>
2021-11-26 10:58:38 +0800

    Update MainApp.py

--- MainApp.py
+++ MainApp.py
@@ -21,16 +21,18 @@ async def venom(ctx):
     await ctx.channel.send("假猛毒")
     vid = Memes.venom()
     await ctx.channel.send(file=discord.File(vid))
-
+    print("Venom called")
 @bot.command()
 async def memes(ctx):
     pic = Memes.get()
     await ctx.channel.send(file=discord.File(pic))
+    print("Memes called")
 
 @bot.command()
 async def jokes(ctx):
     joke_question, joke_answer = Jokes.tell()
     await ctx.reply("{}\n\n{}".format(joke_question, joke_answer))
+    print("Jokes called")
 
 @bot.command()
 async def about(ctx):
@@ -39,11 +41,13 @@ async def about(ctx):
 jokes：隨機產生一個笑話
 memes：隨機傳一張梗圖
 更多功能日後上線（當然，如果我有空的話）""")
+    print("About called")
 
 @bot.command()
 async def hello(ctx):
     mention = ctx.author.mention
     await ctx.channel.send("Hello, {}".format(mention))
+    print("Hello Called")
 
 @bot.command()
 @commands.has_role("Admin")
@@ -57,6 +61,7 @@ async def ban(ctx, member : discord.Member, *, reason = None):
     await member.send(message)
     await member.ban(reason = reason)
     await ctx.channel.send(f"{member}已被relaxing234永久禁言")
+    print("Ban called")
 
 @bot.command()
 @commands.has_role("Admin")
@@ -64,5 +69,6 @@ async def unban(ctx, user : discord.User):
     await ctx.guild.unban(user)
     await user.send(f"您已被{ctx.guild.name} unban")
     await ctx.channel.send(f"機器人大軍已決定赦免{user}")
+    print("Unban called")
 
 bot.run('OTAwNzI2NTkzOTQ3ODQ0NjU5.YXFhAg.-wJ99DG_2l0sV4AcmT8kjz8zBFA')
